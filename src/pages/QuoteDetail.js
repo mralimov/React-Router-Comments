bc5ebcdeda3352106fb72f498b30ad3b087fe294
Donnie Alimov <mralimov89@gmail.com>
2021-08-04 22:28:02 -0400

    HighlightedQuote

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -2,13 +2,25 @@ import React from 'react';
 import { useParams, Route } from 'react-router-dom';
 import { Fragment } from 'react';
 import Comments from '../components/comments/Comments';
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
+
+const DUMMY_DATA = [
+  { id: 'q1', author: 'Don', text: 'Learning React Router' },
+  { id: 'q2', author: 'John', text: 'Learning React Router by John' },
+  { id: 'q3', author: 'Adam', text: 'Learning React Router by Adam' },
+];
 
 function QuoteDetail() {
   const params = useParams();
+
+  const quote = DUMMY_DATA.find((quote) => (quote.id = params.quoteId));
+
+  if (!quote) {
+    return <p>No Quote was found</p>;
+  }
   return (
     <Fragment>
-      <h1>QuoteDetail</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
